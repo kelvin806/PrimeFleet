e2fa8b9bdddde1dc70f0745c9d821a42ef69ded2
kelvin806 <ogenyikelvin3@gmail.com>
2026-03-14 09:12:29 -0700

    added javascript functionality to deposit payment page

--- deposit-payment/desposit.js
+++ deposit-payment/desposit.js
@@ -18,3 +18,36 @@ hamburger.addEventListener("click", function(){
   }
 
 })
+
+
+// Select the radio buttons
+const card = document.getElementById("cardPayment");
+const bank = document.getElementById("bankTransfer");
+
+// Select the sections we want to show or hide
+const coming = document.querySelector(".coming");
+const transfer = document.querySelector(".transfer");
+
+
+// When CARD payment is selected
+card.addEventListener("change", function () {
+
+    // Show the coming soon message
+    coming.style.display = "block";
+
+    // Hide the bank transfer section
+    transfer.style.display = "none";
+
+});
+
+
+// When BANK TRANSFER is selected
+bank.addEventListener("change", function () {
+
+    // Hide the coming soon message
+    coming.style.display = "none";
+
+    // Show the bank transfer section
+    transfer.style.display = "block";
+
+});
